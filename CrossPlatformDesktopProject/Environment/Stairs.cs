4563f296bb4aca81bb737221b47049231240d807
Matthew Landwehr <landwehr.30@buckeyemail.osu.edu>
2020-10-04 17:55:15 -0400

    some environment stuff

--- CrossPlatformDesktopProject/Environment/Stairs.cs
+++ CrossPlatformDesktopProject/Environment/Stairs.cs
@@ -3,10 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
 
 namespace CrossPlatformDesktopProject.Environment
 {
-    class Stairs
+    class Stairs : IBlock
     {
+        public Texture2D Texture { get; set; }
+
+        public Stairs(Texture2D texture)
+        {
+            Texture = texture;
+        }
+
+
+        public void Draw(SpriteBatch spriteBatch, Vector2 location)
+        {
+            Rectangle sourceRectangle;
+            Rectangle destinationRectangle;
+
+            sourceRectangle = new Rectangle(1036, 29, 14, 14);
+            destinationRectangle = new Rectangle(200, 400, 50, 50);
+
+            spriteBatch.Begin();
+            spriteBatch.Draw(Texture, destinationRectangle, sourceRectangle, Color.White);
+            spriteBatch.End();
+        }
     }
 }
--- CrossPlatformDesktopProject/Environment/Statue.cs
+++ CrossPlatformDesktopProject/Environment/Statue.cs
@@ -3,10 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
 
 namespace CrossPlatformDesktopProject.Environment
 {
-    class Statue :IBlock
+    class Statue : IBlock
     {
+        public Texture2D Texture { get; set; }
+
+        public Statue(Texture2D texture)
+        {
+            Texture = texture;
+        }
+
+
+        public void Draw(SpriteBatch spriteBatch, Vector2 location)
+        {
+            Rectangle sourceRectangle;
+            Rectangle destinationRectangle;
+
+            sourceRectangle = new Rectangle(1018, 11, 16, 16);
+            destinationRectangle = new Rectangle(200, 400, 50, 50);
+
+            spriteBatch.Begin();
+            spriteBatch.Draw(Texture, destinationRectangle, sourceRectangle, Color.White);
+            spriteBatch.End();
+        }
     }
 }
