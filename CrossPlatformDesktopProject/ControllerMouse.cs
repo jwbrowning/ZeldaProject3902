bf5e1e698cc04e6899186fe62baad12fe160321b
aHaskett17 <41378663+aHaskett17@users.noreply.github.com>
2020-10-26 11:51:28 -0400

    Update ControllerMouse.cs

--- CrossPlatformDesktopProject/ControllerMouse.cs
+++ CrossPlatformDesktopProject/ControllerMouse.cs
@@ -1,66 +1,34 @@
-﻿using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
 using System;
+using System.Security.Cryptography.X509Certificates;
 
 namespace Sprint0
 {
     class ControllerMouse : IController
     {
-
+        private Game1 game;
         MouseState state;
         //TODO fix these magic numbers, how do i get the screen size?
         int windowWidth = 800;
         int windowHeight = 480;
-        public bool Button0Pressed()
-        {
-            if (state.RightButton == ButtonState.Pressed)
-                return true;
-            else
-                return false;
-        }
-
-        public bool Button1Pressed()
-        {
-            if (state.X < windowWidth / 2 && state.Y < windowHeight / 2)
-            {
-                if (state.LeftButton == ButtonState.Pressed)
-                    return true;
-            }
-            return false;
-        }
-
-        public bool Button2Pressed()
-        {
-            if (state.X >= windowWidth / 2 && state.Y < windowHeight / 2)
-            {
-                if (state.LeftButton == ButtonState.Pressed)
-                    return true;
-            }
-            return false;
-        }
-
-        public bool Button3Pressed()
-        {
-            if (state.X < windowWidth / 2 && state.Y >= windowHeight / 2)
-            {
-                if (state.LeftButton == ButtonState.Pressed)
-                    return true;
-            }
-            return false;
-        }
-
-        public bool Button4Pressed()
-        {
-            if (state.X >= windowWidth / 2 && state.Y >= windowHeight / 2)
-            {
-                if (state.LeftButton == ButtonState.Pressed)
-                    return true;
-            }
-            return false;
+        public ControllerMouse(Game1 game) {
+            this.game = game;           
         }
 
         public void Update()
         {
             state = Mouse.GetState();
+            bool leftPressed = ButtonState.Pressed == state.LeftButton ? true : false;
+            if(leftPressed) {
+                if(state.X>400) {
+                    //load next stage
+                    CommandSword swing = new CommandSword(game);
+                    swing.Execute();
+                } else {
+                    //load prev stage
+                }
+            }
         }
 
         void IController.RegisterCommand(Keys key, ICommand command)
