7918bd322e99c59ba22d282f40a3da7c30d90788
aHaskett17 <41378663+aHaskett17@users.noreply.github.com>
2020-11-15 21:20:13 -0500

    Add files via upload

---
+++ CrossPlatformDesktopProject/Environment/DoorSpriteFactory.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace CrossPlatformDesktopProject.Environment
+{
+    class DoorSpriteFactory
+    {
+
+        public Texture2D environment;
+        private static DoorSpriteFactory instance = new DoorSpriteFactory();
+
+
+        public static DoorSpriteFactory Instance
+        {
+            get
+            {
+                return instance;
+            }
+        }
+        public DoorSpriteFactory()
+        {
+        }
+
+        public void LoadAllTextures(ContentManager content)
+        {
+            environment = content.Load<Texture2D>("environment");
+        }
+    }
+}
